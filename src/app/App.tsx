062ca3edeb858df6865392a440083bdf9eb0da88
Arthur Akhmetov <artur.akhmetov.2008@gmail.com>
2025-01-14 16:01:12 +0300

    add page contact

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,5 @@
 import { Attractions } from 'pages/attractions/Attractions'
+import { Contacts } from 'pages/contacts/Contacts'
 import { Home } from 'pages/home/Home'
 import { Route, Routes } from 'react-router'
 import { Layout } from './Layout'
@@ -10,9 +11,9 @@ const App = () => {
 				<Route path='/' element={<Layout />}>
 					<Route index element={<Home />} />
 					<Route path='attractions' element={<Attractions />} />
-					<Route path='contacts' element={<Home />} />
+					<Route path='contacts' element={<Contacts />} />
 
-					<Route path='template/<::id>' element={<Home />} />
+					{/* <Route path='template/<::id>' element={<Home />} /> */}
 				</Route>
 			</Routes>
 		</>
--- src/pages/contacts/Contacts.tsx
+++ src/pages/contacts/Contacts.tsx
@@ -1,3 +1,42 @@
+import { Link } from 'react-router'
+
 export const Contacts = () => {
-	return <div>Contacts</div>
+	return (
+		<section className='contacts'>
+			<div className='container'>
+				<h2 className='section-title'>Наши контакты</h2>
+				<p className='phone'>
+					рабочий телефон: <Link to={'tel:+79870689005'}>+79870689005</Link>
+				</p>
+				<p className='email'>
+					рабочая почта:
+					<Link to={'mailto:tatsveet@gmail.com'}>tatsveet@gmail.com</Link>
+				</p>
+
+				<button type='button' className='btn'>
+					Связаться с нами
+				</button>
+
+				<div id='myModal' className='modal'>
+					<div className='modal-content'>
+						<form action='#' id='form'>
+							<label>
+								Ваша почта:
+								<input type='email' id='email' name='email' />
+							</label>
+
+							<label>
+								Ваш телефон:
+								<input type='tel' id='tel' name='phone' />
+							</label>
+
+							<button className='btn' id='send'>
+								Отправить
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
